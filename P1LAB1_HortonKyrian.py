2e61a9981eda6069dc7e6212745e04811f964fad
KyrianHorton <kgh122808@gmail.com>
2025-09-22 13:22:35 -0400

    Update P1LAB1_HortonKyrian.py

--- P1LAB1_HortonKyrian.py
+++ P1LAB1_HortonKyrian.py
@@ -1,5 +1,5 @@
-# Your Name
-# Date
+# Kyrian
+# 9-21-2025
 # P1LAB1_HortonKyrian
 # This program asks the user for their first and last name,
 # then prints a welcome message to CTI-110.
@@ -10,3 +10,4 @@ last_name = input("Enter your last name: ")
 
 # Display welcome message
 print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
+
